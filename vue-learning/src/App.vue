7f3d0f3dabe089d45b83e763ffb997ca1d215b75
RomainTessier <romaintessierpro1@gmail.com>
2024-03-17 16:38:02 +0100

    Vue learning base - forms

---
+++ vue-learning/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <SignupForm />
+
+</template>
+
+<script >
+import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignupForm
+  }
+}
+</script>
+
+<style>
+
+</style>
